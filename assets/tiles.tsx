0fe86b44d0a0ff9900c0cb72634c2feb5fe3ecdf
GasTheBrain <sapingaspard@gmail.com>
2026-05-22 16:24:16 +0200

    correction des chemin

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="tiles" tilewidth="32" tileheight="32" tilecount="247" columns="13">
- <image source="../Documents/Epita/Projet S2/tileset.png" width="446" height="628"/>
+ <image source="tileset.png" width="446" height="628"/>
 </tileset>
--- assets/zones7.tsx
+++ assets/zones7.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="zones7" tilewidth="32" tileheight="32" tilecount="2304" columns="48">
- <image source="../../../Users/User/Downloads/plaques_deco_solaris7.png" width="1536" height="1536"/>
+ <image source="plaques_deco_solaris7.png" width="1536" height="1536"/>
 </tileset>
